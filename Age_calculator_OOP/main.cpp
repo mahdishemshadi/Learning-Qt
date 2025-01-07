982b4e9e193bfbbc44edd334211c2b226ffd02df
Mehdishemshadi <mahdi.shemshadi@gmail.com>
2025-01-07 09:38:11 +0330

    Create objects and print them

--- Age_calculator_OOP/main.cpp
+++ Age_calculator_OOP/main.cpp
@@ -1,8 +1,30 @@
 #include <QCoreApplication>
 
+#include "age_calculator.h"
+
+void print(age_calculator& calc){
+
+    qDebug() << "Name:" << calc.getName() << "human years:" << calc.humanYears();
+    qDebug() << "Name:" << calc.getName() << "dog years:" << calc.dogYears();
+    qDebug() << "Name:" << calc.getName() << "catn years:" << calc.catYears();
+
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    age_calculator brayan;
+    age_calculator tammy;
+
+    brayan.setName("Brayan");
+    tammy.setName("Tammy");
+
+    brayan.setAge(26);
+    tammy.setAge(27);
+
+    print(brayan);
+    print(tammy);
 
     return a.exec();
 }
