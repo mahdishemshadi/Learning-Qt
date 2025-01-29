f04e702c60b97b4550f4b8a603240bdb8f189be0
Mehdishemshadi <mahdi.shemshadi@gmail.com>
2025-01-29 09:13:19 +0330

    Done

--- Timer/test.h
+++ Timer/test.h
@@ -2,6 +2,8 @@
 #define TEST_H
 
 #include <QObject>
+#include <QTimer>
+#include <QDebug>
 
 class Test : public QObject
 {
@@ -9,7 +11,17 @@ class Test : public QObject
 public:
     explicit Test(QObject *parent = nullptr);
 
+private:
+    const int m_max_count = 5;
+
+    QTimer m_timer;
+
+    int m_count;
+
 signals:
+
+public slots:
+    void timeout();
 };
 
 #endif // TEST_H
